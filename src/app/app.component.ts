0fbf4043df794a74875d9557f8b1f6b31557bd9f
Lucas Peixoto <descomplicandoapratica@gmail.com>
2024-10-09 22:16:22 -0300

    start config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, UserComponent],
+  template: `
+    <section>
+      <h1>Angular SSR</h1>
+      <app-user></app-user>
+    </section>
+  `,
+  styles: ``
 })
 export class AppComponent {
-  title = 'angular-ssr-tutorial';
+  
 }
---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  template: ``,
+  styles: ``,
+  providers: [UserService]
+})
+export class UserComponent {}
